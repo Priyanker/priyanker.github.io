f5c894fb719355118850f40f3e44ab3d09e00551
Priyanker <priyankerrao@gmail.com>
2024-02-04 19:39:07 -0700

    test: changing Hi to Hey

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,7 +21,7 @@ const illustration = {
 
 const greeting = {
   username: "Priyanker",
-  title: "Hi all, I'm Priyanker",
+  title: "Hey all, I'm Priyanker",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
