e16f943caa971b405d42ec9de1cef1bfba3f31aa
Priyanker <priyankerrao@gmail.com>
2024-02-04 19:24:21 -0700

    removing merge conflicts

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,19 +9,6 @@ export default function Footer() {
   return (
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
-<<<<<<< HEAD
-        
-=======
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by DeveloperFolio Team")}
-        </p>
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          Theme by{" "}
-          <a href="https://github.com/saadpasta/developerFolio">
-            developerFolio
-          </a>
-        </p>
->>>>>>> parent of 6c5bfd5 (Updates)
       </div>
     </Fade>
   );
